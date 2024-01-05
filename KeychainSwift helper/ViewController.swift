b081cccb17ca0a420c96761c8c443e7ca2fab56f
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-01-05 22:57:29 +0100

    Testing out KeychainSwift helper

--- KeychainSwift helper/ViewController.swift
+++ KeychainSwift helper/ViewController.swift
@@ -12,8 +12,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Testing out KeychainSwift helper
+        
+        Keychain.setPassword(password: "JMB234!GS")
+        // Keychain.deletePassword()
+        let password = Keychain.getPassword()
+        if let password = password {
+            print("Your password is: \(password)\n")
+        } else {
+            print("Your password was not set.\n")
+        }
+        
+        Keychain.setUser(user: User(name: "John",
+                                    surname: "Frisby",
+                                    id: "IMB12L",
+                                    age: 32))
+        // Keychain.deleteUser()
+        let user = Keychain.getUser()
+        if let user = user {
+            print(user)
+        }
+        else {
+            print("There is no user.\n")
+        }
     }
-
-
 }
 
