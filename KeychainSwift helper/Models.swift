bb9f815b471fd07fab7933f4adcbddb6de2f4054
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-01-05 22:43:01 +0100

    Added a file with models

---
+++ KeychainSwift helper/Models.swift
@@ -0,0 +1,15 @@
+//
+//  Models.swift
+//  KeychainSwift helper
+//
+//  Created by Macbook Air 2017 on 5. 1. 2024..
+//
+
+import Foundation
+
+struct User: Codable {
+    let name: String
+    let surname: String
+    let id: String
+    let age: Int
+}
